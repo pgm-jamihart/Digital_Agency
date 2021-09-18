1cae7b3228ef387f327408eb5dd56c57f9f8a0b8
Jamie-Lee Hart <71268169+pgm-jamihart@users.noreply.github.com>
2021-09-18 15:51:20 +0200

    initial commit

---
+++ site/_static/js/main.js
@@ -0,0 +1,32 @@
+(() => {
+  const app = {
+    initialize () {
+      this.cacheElements();
+      this.registerEventListeners();
+    },
+    cacheElements () {
+      this.btnToTopElement = document.querySelector('.btn-to-top');
+      this.hamburgerButton = document.getElementById('hamburger-icon');
+      this.navOverlay = document.getElementById('nav-overlay');
+    },
+    registerEventListeners () {
+      if (this.btnToTopElement !== null) {
+        this.btnToTopElement.addEventListener('click', (ev) => {
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          });
+        });
+      }
+
+      if (this.hamburgerButton !== null) {
+        this.hamburgerButton.addEventListener('click', (e) => {
+          this.hamburgerButton.classList.toggle('open');
+          this.navOverlay.classList.toggle('nav-overlay--open');
+        });
+      }
+    },
+  };
+  app.initialize();
+})();
